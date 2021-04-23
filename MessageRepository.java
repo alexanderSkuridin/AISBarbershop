38eb7619244fe68fd2ac27c7cd721fdd30d792de
Alexander <55065762+alexanderSkuridin@users.noreply.github.com>
2021-04-23 08:04:52 +0300

    Delete MessageRepository.java

--- MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ais.barbershop.springbootbarbershopais;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository  extends JpaRepository<Message, Integer> {
-}
